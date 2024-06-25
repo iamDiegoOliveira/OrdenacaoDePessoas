415f20c37218afcc8cadaa4d150d9d68dafef15c
iamDiegoOliveira <diegods1505@gmail.com>
2024-06-25 10:05:49 -0300

    Initial commit

---
+++ src/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,34 @@
+package Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+
+    List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoas(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+
+        return pessoasPorAltura;
+    }
+
+}
